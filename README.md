8f09f03e9611b3b5675164c8e654319283f07bae
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-09-19 03:22:55 -0400

    Working Product, needs potential CSS work and possibly some additional bonus material

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # GitTastic
-Homework 6 - Involving GIPHY API, JavaScript, and jQuery
+Homework 6 - Involving GIPHY API, JavaScript, and jQuery.
+
+## Getting Started
+
+This is a fun and interactive dynamic webpage that uses the GIPHY API through JavaScript and jQuery to update the HTML of the site.
+
+### Prerequisites
+
+The only thing you need to enjoy the fun of the game is a web browser.
+
+```
+Web Browser - Google Chrome Preferred
+```
+
+### Installing
+
+No installation necessary.
+
+## Built With
+
+* [GIPHY API](https://developers.giphy.com/) - API
+* [Meyerweb Reset CSS](https://meyerweb.com/eric/tools/css/reset/) - As the Reset CSS Stylesheet
+* [WebStorm](https://www.jetbrains.com/webstorm/) - IDE
+
+## Authors
+
+* **Dalton Ricker** - *Primary Author* - [SasquatchXYZ](https://github.com/SasquatchXYZ)
+
+## Acknowledgments
+* Many thanks to my instructors & TAs, as well as the O'Reilly reference books.
---
+++ assets/javascript/app.js
@@ -0,0 +1,111 @@
+let topics = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "ferret", "turtle",
+    "sugar glider", "chinchilla", "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken", "capybara",
+    "teacup pig", "serval", "ostrich", "salamander", "frog"];
+
+function renderButtons() {
+    $(".button-container").empty()
+        .html(
+            topics.map(function(topic) {
+                return `<button class="topic-btn" data-topic="${topic}">${topic}</button>`
+            })
+                .join(``)
+        );
+}
+
+$(document).on("click", ".topic-btn", function() {
+    $(".gif-container").empty();
+    var chosenTopic = $(this).attr("data-topic");
+    console.log(chosenTopic);
+
+    let queryURL = `https://api.giphy.com/v1/gifs/search?q=${chosenTopic}&api_key=9OMI8GxD4qrbVLJ6Env1e5XXHytw7ki0&limit=10`;
+
+    $.ajax({
+        url:queryURL,
+        method: "GET"
+    })
+        .then(function(response) {
+            let giffs = response.data;
+            console.log(response.data);
+
+            for (var k = 0; k < giffs.length; k++) {
+                let gifDiv = $(`<div class="gif-cards">`);
+
+                let rating = (giffs[k].rating).toUpperCase();
+
+                let p = $(`<p>`).text(`Rating: ${rating}`);
+
+                let gifImage = $(`<img>`);
+                gifImage.attr("src", giffs[k].images.fixed_height_still.url)
+                    .attr("data-still", giffs[k].images.fixed_height_still.url)
+                    .attr("data-animate", giffs[k].images.fixed_height.url)
+                    .attr("data-state", "still")
+                    .attr("class", "gif");
+
+                gifDiv.prepend(gifImage);
+                gifDiv.prepend(p);
+
+                $(".gif-container").prepend(gifDiv);
+            }
+        }).catch(console.log);
+})
+    .on("mouseover", ".gif", function() {
+        const state = $(this).attr("data-state");
+
+        if (state === 'animate') {
+            const url = $(this).attr("data-still");
+            $(this).attr("data-state", "still");
+            $(this).attr("src", url);
+        } else {
+            const url = $(this).attr("data-animate");
+            $(this).attr("data-state", "animate");
+            $(this).attr("src", url);
+        }
+    });
+
+$(document).ready(function() {
+
+    $("#add-topic").on("click", function(event) {
+
+        event.preventDefault();
+
+        const newTopic = $("#topic-input").val();
+        topics.push(newTopic);
+
+        renderButtons();
+    });
+
+    renderButtons();
+
+/*    $(".topic-btn").on("click", function() {
+        var chosenTopic = $(this).attr("data-topic");
+        console.log(chosenTopic);
+
+        let queryURL = `https://api.giphy.com/v1/gifs/search?q=${chosenTopic}&api_key=9OMI8GxD4qrbVLJ6Env1e5XXHytw7ki0limit=10`
+
+        $.ajax({
+            url:queryURL,
+            method: "GET"
+        })
+            .then(function(response) {
+                let giffs = response.data;
+                console.log(response.data);
+
+            for (var k = 0; k < giffs.length; k++) {
+                let gifDiv = $("<div>");
+
+                let rating = (giffs[k].rating).toUpperCase();
+
+                let p = $("<p>").text(`Rating: ${rating}`);
+
+                let gifImage = $("<img>");
+                gifImage.attr("src", giffs[k].images.fixed_height.url);
+
+                gifDiv.prepend(p);
+                gifDiv.prepend(gifImage);
+
+                $(".gif-container").prepend(gifDiv);
+            }
+        }).catch(console.log);
+    });*/
+
+});
