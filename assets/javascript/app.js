7cd67f183c78c1bb4b63ac6d895eed00acfbd274
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-10-09 00:02:06 -0400

    attempting to set up dotenv

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,8 @@
+/*require()
+console.log(process.env.API_KEY);
+var apiKey = process.env.API_KEY;*/
+
+
 let topics = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "ferret", "turtle",
     "sugar glider", "chinchilla", "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken", "capybara",
     "teacup pig", "serval", "ostrich", "salamander", "frog"];
